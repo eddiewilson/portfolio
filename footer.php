74bd5f7e213a77217a0e7f0d79586b18298f95d5
Edward Wilson <eddie_wilson@hotmail.co.uk>
2016-09-08 19:16:31 +0100

    Base php files

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+	<nav id="social" class="nav social">
+	
+</nav>
+</footer>
+
+
+    <!--[if lt IE 9]>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
+    <!--<![endif]-->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <!--  Contentful -->
+    <script src="https://npmcdn.com/contentful@latest/browser-dist/contentful.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+    <script type="text/javascript">
+		$(document).onload(function(){
+			$(".projects-wrapper").slick({
+				dots: true,
+				infinite: true,
+				speed: 500,
+				fade: true,
+				cssEase: 'linear'
+			});
+		});
+    </script>
+	<script type="text/javascript" src="build/js/all.min.js"></script> 
+  </body>
+</html>
---
+++ grid.php
@@ -0,0 +1,5 @@
+<?php // Some stuff ?>
+<div id="projects-wrapper" class="projects-wrapper">
+	
+</div>
+<div id="content"></div>
