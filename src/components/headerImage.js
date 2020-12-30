a33f8b0ff074cd6084aaea8898de23fb103f6171
Edward Wilson <ed@lighthouseandgiant.co.uk>
2020-12-30 21:12:35 +0000

    some motion

--- src/components/headerImage.js
+++ src/components/headerImage.js
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { graphql, StaticQuery } from "gatsby"
-import { Typography, withTheme } from "@material-ui/core/"
+import { Typography, withTheme, Fade } from "@material-ui/core/"
 import styled from "styled-components"
 import { rhythm } from "../utils/typography"
 import BackgroundImage from "gatsby-background-image"
 
-const Heading = withTheme(styled(Typography)`
+const Heading = withTheme(styled.div.attrs({
+  className: "doAnimation",
+})`
   position: absolute;
   left: 11vw;
   bottom: ${rhythm(4)};
+  z-index: 1;
+  ${props => props.theme.breakpoints.up("sm")} {
+    max-width: 75vw;
+    bottom: ${rhythm(4)};
+  }
+`)
+
+const TopHeading = withTheme(styled(Typography).attrs({
+  className: "topHeading",
+})`
+  color: white;
+  z-index: 1;
+  ${props => props.theme.breakpoints.up("sm")} {
+    max-width: 75vw;
+    bottom: ${rhythm(4)};
+  }
+`)
+
+const BottomHeading = withTheme(styled(Typography).attrs({
+  className: "bottomHeading",
+})`
   color: white;
   z-index: 1;
   ${props => props.theme.breakpoints.up("sm")} {
@@ -33,42 +56,68 @@ const HeroImage = styled(BackgroundImage)`
   }
 `
 
-const BackgroundSection = ({ className, props }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        desktop: file(
-          relativePath: { eq: "Clays_Adult_Hand_iPhone_6s_Presentation_1.jpg" }
-        ) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_withWebp
+const BackgroundSection = ({ className, props }) => {
+  const [topAnimation, setTopAnimation] = useState(false)
+  const [bottomAnimation, setBottomAnimation] = useState(false)
+
+  const doAnimation = () => {
+    setTopAnimation(true)
+    setTimeout(function () {
+      setBottomAnimation(true)
+    }, 1300)
+  }
+
+  useEffect(doAnimation, [])
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          desktop: file(
+            relativePath: {
+              eq: "Clays_Adult_Hand_iPhone_6s_Presentation_1.jpg"
+            }
+          ) {
+            childImageSharp {
+              fluid(quality: 90, maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
             }
           }
         }
-      }
-    `}
-    render={data => {
-      // Set ImageData.
-      const imageData = data.desktop.childImageSharp.fluid
-      return (
-        <StyledBackgroundSection>
-          <HeroImage
-            Tag="section"
-            className={className}
-            fluid={imageData}
-            backgroundColor={`#ededed`}
-          >
-            <Overlay />
-            <Heading variant="h1" component="h2">
-              I’m Ed — <br />a developer / designer based in Cornwall
-            </Heading>
-          </HeroImage>
-        </StyledBackgroundSection>
-      )
-    }}
-  />
-)
+      `}
+      render={data => {
+        // Set ImageData.
+        const imageData = data.desktop.childImageSharp.fluid
+        return (
+          <StyledBackgroundSection>
+            <HeroImage
+              Tag="section"
+              className={className}
+              fluid={imageData}
+              backgroundColor={`#ededed`}
+            >
+              <Overlay />
+
+              <Heading>
+                <Fade in={topAnimation}>
+                  <TopHeading variant="h1" component="h2">
+                    I’m Ed —
+                  </TopHeading>
+                </Fade>
+                <br />
+                <Fade in={bottomAnimation}>
+                  <BottomHeading variant="h1" component="h2">
+                    a developer / designer based in Cornwall
+                  </BottomHeading>
+                </Fade>
+              </Heading>
+            </HeroImage>
+          </StyledBackgroundSection>
+        )
+      }}
+    />
+  )
+}
 
 const StyledBackgroundSection = styled.div`
   .header-background {
